8bbed5773dda1bd7eed0d4544f9d55f207d35b22
toritotony <wolfeanthony584@gmail.com>
2025-10-01 18:55:06 -0700

    Comment postback form and add db connection file that we'll need

---
+++ project1/client-app/form_page.php
@@ -0,0 +1,6 @@
+<!--
+    This section is part of our plan to create a callback form that utilizes the database
+    to dynamically populate selectable choices within the form. The options presented to the
+    Upon submission, we will leverage postback form techniques to update the page appearance,
+    providing immediate feedback or additional options based on user input.
+-->
